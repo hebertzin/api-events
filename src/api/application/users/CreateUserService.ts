5839d338f2876d8c9b6ca45179deba39871fa68b
hebertsanto <hebertsantos0704@gmail.com>
2024-07-11 10:25:24 -0300

    feat : Implement hash service

--- src/api/application/users/CreateUserService.ts
+++ src/api/application/users/CreateUserService.ts
@@ -1,4 +1,6 @@
-import { Service } from "../../domain/service";
+import { Service } from "../../domain/Service";
+import { UserAlreadyExist } from "../../errors/errors";
+import { BcryptHashService } from "../../infraestructure/bcrypt/BcryptHashService";
 import { UsersRepositoryImpl } from "../../infraestructure/db/repository/users/UsersRepositoryImpl";
 
 type CreateUserRequest = {
@@ -14,18 +16,33 @@ type CreateUserResponse = {
 export class CreateUserService
   implements Service<CreateUserRequest, CreateUserResponse>
 {
-  constructor(private readonly usersRepository: UsersRepositoryImpl) {}
+  constructor(
+    private readonly usersRepository: UsersRepositoryImpl,
+    private readonly bcrypt: BcryptHashService,
+  ) {}
 
   async invoke({
     name,
     email,
     password,
   }: CreateUserRequest): Promise<CreateUserResponse> {
+    const existentUser = await this.usersRepository.findByEmail(email);
+
+    if (existentUser) {
+      throw new UserAlreadyExist("Usuário já existe", 209);
+    }
+
     try {
-      const user = await this.usersRepository.create({ email, name, password });
+      const passwordHashed = await this.bcrypt.hash(password);
+
+      const user = await this.usersRepository.create({
+        email,
+        name,
+        password: passwordHashed,
+      });
       return { user };
     } catch (error) {
-      throw new Error("Some error has been ocurred...");
+      throw new Error("Erro interno do servidor");
     }
   }
 }
