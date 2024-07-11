343ef814850011807154a0ad68d62ca6bb503340
hebertsanto <hebertsantos0704@gmail.com>
2024-07-11 11:51:19 -0300

    chore : config users dependencie

--- src/api/infraestructure/express/controllers/users/CreateUserController.ts
+++ src/api/infraestructure/express/controllers/users/CreateUserController.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from "express";
 import { CreateUserService } from "../../../../application/users/CreateUserService";
+import { HttpStatusCode } from "../../../utils/HttpStatusCode";
+import { z } from "zod";
+
+export const zodValidationUserSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  password: z.string(),
+});
 
 export class CreateUserController {
   constructor(private usersService: CreateUserService) {}
 
   async handle(req: Request, res: Response): Promise<Response> {
+    const { email, password, name } = zodValidationUserSchema.parse(req.body);
     try {
       await this.usersService.invoke({
-        email: req.body.email,
-        name: req.body.name,
-        password: req.body.password,
+        email: email,
+        name: name,
+        password: password,
       });
-      return res.status(201).json({ msg: "User created" });
+      return res
+        .status(HttpStatusCode.Created)
+        .json({ msg: "Usuário criado com sucesso" });
     } catch (error) {
-      return res.status(500).json({ msg: "Internal server error" });
+      return res.status(error.code).json({ msg: error.message });
     }
   }
 }
