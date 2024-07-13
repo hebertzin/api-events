bc2cbb3777d9d8a47743802afbd966da2aa19768
hebertsanto <hebertsantos0704@gmail.com>
2024-07-13 10:57:51 -0300

    chore : Add correct importation

--- src/tests/e2e/activity/DeleteActivity.test.ts
+++ src/tests/e2e/activity/DeleteActivity.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
-import { ExpressApp as app } from "../../../src/app";
-import { prisma } from "../../../src/api/infraestructure/db/client/PrismaClient";
+import { ExpressApp as app } from "../../../app";
+import { prisma } from "../../../api/infraestructure/db/client/PrismaClient";
 
 describe("Delete activity", () => {
   it("Should delete an activity", async () => {
@@ -15,7 +15,7 @@ describe("Delete activity", () => {
     });
 
     const response = await request(new app().getApp()).delete(
-      "/api/v1/activity/5a1eab11-19f5-4c00-918a-cb5358a68beb",
+      "/api/v1/activity/5a1eab11-19f5-4c00-918a-cb5358a68beb"
     );
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("Activity deleted sucessfully");
--- src/tests/e2e/activity/GetActivity.test.ts
+++ src/tests/e2e/activity/GetActivity.test.ts
@@ -4,7 +4,7 @@ import { ExpressApp as app } from "../../../app";
 describe("Get activity", () => {
   it("Should get an actvity", async () => {
     const response = await request(new app().getApp()).get(
-      "/api/v1/activity/a0d7d41f-acb8-4eb1-9157-5869162e3839"
+      "/api/v1/activity/a0d7d41f-acb8-4eb1-9157-5869162e3839",
     );
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("Get activity sucessfully");
