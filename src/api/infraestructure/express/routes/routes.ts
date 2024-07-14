eb7814156a50ff9842fc9e70fb0261ec0f0d7a27
hebertsanto <hebertsantos0704@gmail.com>
2024-07-14 12:08:54 -0300

    feat : Create auth middleware

--- src/api/infraestructure/express/routes/routes.ts
+++ src/api/infraestructure/express/routes/routes.ts
@@ -2,9 +2,14 @@ import { Router } from "express";
 import { userRoutes } from "./users/UserRoutes";
 import { authRoutes } from "./authentication/AuthRoutes";
 import { activiyRoutes } from "./activity/ActivityRoutes";
+import { authMiddleware } from "../middlewares";
 
 export const AppRoutes = Router();
 
 AppRoutes.use("/users", userRoutes);
 AppRoutes.use("/authentication/user", authRoutes);
-AppRoutes.use("/activity", activiyRoutes);
+AppRoutes.use(
+  "/activity",
+  authMiddleware.isAuthorized.bind(authMiddleware),
+  activiyRoutes
+);
