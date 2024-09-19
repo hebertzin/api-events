2ce77058fb057653b30443b1b5011ddd406a225c
hebertsanto <hebertsantos0704@gmail.com>
2024-09-19 09:58:37 -0300

    feat : add activity routes

--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userManagementRoutes } from "./users/user-routes";
 import { authRoutes } from "./authentication/auth-routes";
+import { activitiesManagementRoutes } from "./activity/activty-routes";
 
 export const router = Router();
 
 router.use("/users", userManagementRoutes);
+router.use("/activity", activitiesManagementRoutes);
 router.use("/authentication/user", authRoutes);
---
+++ src/presentation/validators/activity-validator.ts
@@ -0,0 +1,9 @@
+import { activityValidationSchema } from "./schemas/activity";
+import {
+  DataValidator,
+  ZodValidator,
+} from "../middlewares/zod-validator-middleware";
+
+export const activityValidatorMiddleware = new DataValidator(
+  new ZodValidator(activityValidationSchema)
+);
